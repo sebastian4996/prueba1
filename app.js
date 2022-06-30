1bb9bdbc98af2a4cefb0bf7ab54fba957c57691e
sebastian4996 <sebperjuan@gmail.com>
2022-06-30 14:31:50 -0500

    primer git

---
+++ app.js
@@ -0,0 +1,27 @@
+//USO DE VARIABLES
+/*var nombre = 'Juan';
+console.log(nombre);
+console.log(typeof(nombre));
+
+var edad=35;
+console.log(edad);
+console.log(typeof(edad));
+
+var sueldo=1900.99;
+console.log(sueldo);
+console.log(typeof(sueldo));
+
+var tieneTrabajo = false;
+console.log(tieneTrabajo);
+console.log(typeof(tieneTrabajo));
+
+var puestotrabajo;
+console.log(puestotrabajo);
+console.log(typeof(tieneTrabajo));
+
+puestotrabajo=null;
+console.log(puestotrabajo);
+
+edad = 'cinco';
+console.log(edad);
+console.log(typeof(edad));*/
